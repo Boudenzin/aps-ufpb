cde2ea6216cf7c17cfd0052e2b137a8f27714a6f
Romildo Júnior <romildo-rrj@hotmail.com>
2025-11-19 16:34:36 -0300

    feat: Implementa o balanceador de carga

--- JavalinRAM/src/main/java/org/example/Main.java
+++ JavalinRAM/src/main/java/org/example/Main.java
@@ -7,6 +7,25 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> {
+
+            String serverId = System.getenv("SERVER_ID");
+
+            if (serverId == null) {
+                serverId = "UNKNOWN";
+            }
+
+            ctx.result("Resposta do Servidor ID: " + serverId);
+
+        });
     }
 }
+/*
+Exemplo de comando 'for' no Windows para fazer 100 requisições sequenciais
+for /l %i in (1,1,100) do curl http://localhost:7000/
+
+Para rodar o Jconsole: "C:\Program Files\Java\jdk-25\bin\jconsole.exe"
+
+docker run -d --name meu-nginx -p 80:80 -v "%cd%/nginx.conf:/etc/nginx/nginx.conf" nginx
+ */
+
